171aa5bc1b099876e1cd94176f60b58581391df1
Julio Montoya <gugli100@gmail.com>
2010-07-05 12:02:56 +0200

    Modifying the Invoices

--- client/pages/invoices.php
+++ client/pages/invoices.php
@@ -10,44 +10,119 @@
 if(THT != 1){die();}
 
 class page {
-	public function content(){ # Displays the page 
-		global $style, $db, $main, $invoice, $server;
-		if($_GET['invoiceID']){
-			require_once("../includes/paypal/paypal.class.php");
-			$paypal = new paypal_class;
-			if($paypal->validate_ipn()){
-				$invoice->set_paid(mysql_real_escape_string($_GET['invoiceID']));
-				$main->errors("Your invoice has been paid!");
-				$client = $db->fetch_array($db->query("SELECT * FROM `<PRE>user_packs` WHERE `userid` = '{$_SESSION['cuser']}'"));
-				if($client['status'] == 2) {
-					$server->unsuspend($client['id']);
+	
+	public function __construct() {
+		$this->navtitle = "Invoice menu";		
+		$this->navlist[] = array("View all invoices", "page_white_go.png", "all");
+	}
+	
+	public function description() {
+		return "<strong>Invoices Area</strong><br />
+		This is the area where you can view invoices.";	
+	}	
+	
+	public function content() {
+		global $style, $db, $main, $invoice, $server, $addon, $billing;
+
+		switch($main->getvar['sub']) {
+			case 'paid':				
+				if(isset($_GET['invoiceID'])) {					
+					require_once("../includes/paypal/paypal.class.php");
+					$paypal = new paypal_class;
+					if($paypal->validate_ipn()){
+						$invoice->set_paid(intval($_GET['invoiceID']));
+						$main->errors("Your invoice has been paid!");
+						$client = $db->fetch_array($db->query("SELECT * FROM `<PRE>user_packs` WHERE `userid` = '{$_SESSION['cuser']}'"));
+						if($client['status'] == 2) {
+							$server->unsuspend($client['id']);
+						}
+					}
+					else {
+						$main->errors("Your invoice hasn't been paid!");
+					}
 				}
-			}
-			else {
-				$main->errors("Your invoice hasn't been paid!");
-			}
+			break;				
+			case 'view':				
+				if(isset($main->getvar['do'])) {					
+					$return_array = $invoice->getInvoice($main->getvar['do'], true);									
+					echo $style->replaceVar("tpl/invoices/viewinvoice.tpl", $return_array);					
+				}
+			break;					
+			case 'all':
+			default :	
+						
+				//Package info
+				$sql = "SELECT id, name  FROM `<PRE>packages`";
+				$packages 		= $db->query($sql);
+				while ($data = $db->fetch_array($packages)) {
+					$package_name_list[$data['id']] = $data['name'];
+				}
+				
+				$billing_cycle_name_list = $billing->getBillingCycles();
+				
+				//Addons
+				$addons_list = $addon->getAllAddons();
+				
+				
+				
+				// List of invoices. :)
+				$query = $db->query("SELECT * FROM `<PRE>invoices` WHERE `uid` = '{$_SESSION['cuser']}' ORDER BY `id` ASC");
+				$userdata = mysql_fetch_row($db->query("SELECT `user`,`firstname`,`lastname` FROM `<PRE>users` WHERE `id` = {$_SESSION['cuser']}"));
+				$domain = mysql_fetch_row($db->query("SELECT domain, pid, billing_cycle_id  FROM `<PRE>user_packs` WHERE `userid` = {$_SESSION['cuser']}"));
+				$extra = array(
+					"userinfo" 	=> "$userdata[2], $userdata[1] ($userdata[0])",
+					"domain"	=> $domain[0],
+					"pid"	=> $domain[1],
+					"billing_cycle_id"	=> $domain[2]			
+				);
+				$array2['list'] = "";
+				while($array = $db->fetch_array($query)) {
+					$total_amount = 0;				
+					
+					$array['due'] = strftime("%D", $array['due']);
+					$array['paid'] = ($array["is_paid"] == 1 ? "<span style='color:green;font-size:20px;'>Paid</span>" :
+					"<span style='color:red;font-size:20px;'>Unpaid</span>");
+					$array['pay'] = ($array["is_paid"] == 0 ? 
+					'<input type="button" name="pay" id="pay" value="Pay Now" onclick="doswirl(\''.$array['id'].'\')" />' : 'Already paid');
+					
+					$package_id 	  = $extra['pid'];
+					$billing_cycle_id = $extra['billing_cycle_id'];
+					
+					$addon_fee_string = '';
+					if (!empty($array['addon_fee'])) {
+						
+						$array['addon_fee'] = unserialize($array['addon_fee']);
+						
+						foreach($array['addon_fee'] as $addon) {					
+							$addon_fee_string.= $addons_list[$addon['addon_id']].' - '.$addon['amount'].'<br />';
+							$total_amount +=$addon['amount'];					
+						}					
+					}
+					
+					$array['addon_fee'] = $addon_fee_string;
+					
+					
+					$total_amount 		= $total_amount + $array['amount'];
+					$array['amount'] 	= $total_amount." ".$db->config("currency");
+					
+					//$array['package']		 = $package_name_list[$package_id];
+					//$array['billing_cycle']  = $billing_cycle_name_list[$billing_cycle_id]['name'];
+					//$array['due'] =  ($array["is_paid"] == 1 ? '<span style="color:green">'.$array['due'].'</span>' :  '<span style="color:red">'.$array['due'].'</span>');
+					
+					$array['edit']  	= '';			
+					$array['delete']  	= '';
+					
+					
+					//var_dump($array);
+					
+					
+					$array2['list'] .= $style->replaceVar("tpl/invoices/invoice-list-item.tpl", array_merge($array, $extra));
+				}
+				$array2['num'] = mysql_num_rows($query);
+				
+				echo $style->replaceVar("tpl/invoices/client-page.tpl", $array2);
+				break;			
 		}
-		// List invoices. :)
-		$query = $db->query("SELECT * FROM `<PRE>invoices` WHERE `uid` = '{$_SESSION['cuser']}' ORDER BY `id` ASC");
-		$userdata = mysql_fetch_row($db->query("SELECT `user`,`firstname`,`lastname` FROM `<PRE>users` WHERE `id` = {$_SESSION['cuser']}"));
-		$domain = mysql_fetch_row($db->query("SELECT `domain` FROM `<PRE>user_packs` WHERE `userid` = {$_SESSION['cuser']}"));
-		$extra = array(
-			"userinfo" 	=> "$userdata[2], $userdata[1] ($userdata[0])",
-			"domain"	=> $domain[0]
-		);
-		$array2['list'] = "";
-		while($array = $db->fetch_array($query)){
-			$array['due'] = strftime("%D", $array['due']);
-			$array["paid"] = ($array["is_paid"] == 1 ? "<span style='color:green;font-size:20px;'>Paid</span>" :
-			"<span style='color:red;font-size:20px;'>Unpaid</span>");
-			$array["pay"] = ($array["is_paid"] == 0 ? 
-			'<input type="button" name="pay" id="pay" value="Pay Now" onclick="doswirl(\''.$array['id'].'\')" />' :
-			'');
-			$array['amount'] = $array['amount']." ".$db->config("currency");
-			$array2['list'] .= $style->replaceVar("tpl/invoices/invoice-list-item.tpl", array_merge($array, $extra));
-		}
-		$array2['num'] = mysql_num_rows($query);
-		echo $style->replaceVar("tpl/invoices/client-page.tpl", $array2);
 	}
 }
 ?>
--- includes/tpl/invoices/admin-page.tpl
+++ includes/tpl/invoices/admin-page.tpl
@@ -8,5 +8,20 @@
 		<strong>Unpaid Invoices:</strong> %numunpaid%
 	</div>
 </div>
-<br />
-%list%
+
+<table width="100%" border="0" cellspacing="3" cellpadding="0">
+	<tr> 
+        <td width="5%"><div align="left"><b>&nbsp;No.</b></div></td>
+        <td><strong>User</strong></td>
+        <td><strong><a title="Domain name" class="tooltip"><img src="<ICONDIR>world.png" border="0" /></a> Domain</strong></td>
+        <td><strong><a title="The amount of money you owe." class="tooltip"><img src="<ICONDIR>money.png" border="0" /></a> Amount</strong></td>      	
+        <!--  <td><b><a title="Package name" class="tooltip"><img src="<ICONDIR>package_green.png" border="0" /></a> Package</b></td>  --> 
+         <!--  <td><a title="Billing cycle" class="tooltip"><img src="<ICONDIR>information.png" border="0" /></a> <strong>Billing cycle</strong></td>  -->
+        <!-- <td><b>Addon List</b></td> -->
+        <!--  -->
+        <td><strong>Status</strong></td>
+        <td><strong><a title="When it's due." class="tooltip"><img src="<ICONDIR>time.png" border="0" /></a> Due date</strong></td>
+        <td width="150px"><strong>Actions</strong></td>                
+	</tr>
+	%list%
+</table>
