d8b5e30f52fcbd36c52c4d4365eb1204664d53cc
Julio <jmontoyaa@users.noreply.github.com>
2010-07-05 12:03:57 +0200

    Updating client view with new changes (Invoices, order, etc)

--- client/pages/view.php
+++ client/pages/view.php
@@ -12,19 +12,26 @@ if(THT != 1){die();}
 class page {
 	
 	public function content() { # Displays the page 
-		global $style, $db, $main, $server;
+		global $style, $db, $main, $server, $invoice;
 		$data = $db->client($_SESSION['cuser']);
 		$query2 = $db->query("SELECT * FROM `<PRE>user_packs` WHERE `userid` = '{$db->strip($data['id'])}'");
 		$data3 = $db->fetch_array($query2);
 		$query = $db->query("SELECT * FROM `<PRE>packages` WHERE `id` = '{$db->strip($data3['pid'])}'");
 		$data2 = $db->fetch_array($query);		
 		$query3 = $db->query("SELECT * FROM `<PRE>users` WHERE `id` = '{$db->strip($data['id'])}'");
-		$data4 = $db->fetch_array($query3);
-		$array['USER'] = $data4['user'];
+		$user_info = $db->fetch_array($query3);
+		
+		$array['USER'] = $user_info['user'];
 		$array['SIGNUP'] = strftime("%D", $data3['signup']);
 		$array['DOMAIN'] = $data3['domain'];
 		$array['PACKAGE'] = $data2['name'];
 		$array['DESCRIPTION'] = $data2['description'];
+		
+		$invoice_info = $invoice->getLastInvoiceByUser($user_info['id']);
+		
+		$return_array = $invoice->getInvoice($invoice_info['id'], true, false);
+		$array['ADDON'] = $return_array['ADDON'];
+		//var_dump($return_array);
 		
 		if($_POST) {
 				if(md5(md5($main->postvar['currentpass']) . md5($data['salt'])) == $data['password']) {
