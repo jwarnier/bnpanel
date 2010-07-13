1b686e49646561cb2371817bf6ccd7838bba0fea
Julio Montoya <gugli100@gmail.com>
2010-07-13 12:19:28 +0200

    Minor - cosmetic changes

--- client/confirm.php
+++ client/confirm.php
@@ -7,27 +7,22 @@ include(LINK ."compiler.php");
 //THT Variables
 define("PAGE", "Confirm");
 
-global $main;
-global $server;
-global $style;
+global $main, $server, $style;
 
 echo $style->get("header.tpl"); #Output Header
 echo '<div align="center">';
-
-		if(!$main->getvar['u']) {
-			echo "Please use the link provided in your e-mail.";
-		}
-		else {
-			$username = $main->getvar['u'];
-			$confirm = $main->getvar['c'];
-			$command = $server->confirm($username, $confirm);
-			if($command == false) {
-				echo 'Confirmation failed, please try to copy and paste the link into your browser.';
-			}
-			else {
-				echo 'Account confirmed.';
-			}
-		}
+if(!isset($main->getvar['u'])) {
+	echo "Please use the link provided in your e-mail.";
+} else {
+	$username 	= $main->getvar['u'];
+	$confirm 	= $main->getvar['c'];
+	$command 	= $server->confirm($username, $confirm);	
+	if($command == false) {
+		echo 'Confirmation failed, please try to copy and paste the link into your browser.';
+	} else {
+		echo 'Account confirmed.';
+	}
+}
 echo '</div>'; #End it
 echo $style->get("footer.tpl"); #Output Footer
 
