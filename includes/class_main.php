976841c403c29a42c17446cd31d9dbb4fc94c046
Julio Montoya <gugli100@gmail.com>
2010-07-21 17:50:58 +0200

    Cleaning main class, removing the InsertLog message replace for the addLog message

--- includes/class_main.php
+++ includes/class_main.php
@@ -1,8 +1,8 @@
-<?php/* For licensing terms, see /license.txt */
-//Check if called by script
-if(THT != 1){die();}class main {
-	public $postvar = array(), $getvar = array(); # All post/get strings	
-	public function cleaninteger($var){ # Transforms an Integer Value (1/0) to a Friendly version (Yes/No)
+<?php/* For licensing terms, see /license.txt */
+//Check if called by script
+if(THT != 1){die();}class main {
+	public $postvar = array(), $getvar = array(); # All post/get strings
+	public function cleaninteger($var){ # Transforms an Integer Value (1/0) to a Friendly version (Yes/No)
 	     $patterns[0] = '/0/';
          $patterns[1] = '/1/';
          $replacements[0] = 'No';
@@ -23,14 +23,13 @@ if(THT != 1){die();}class main {
 			echo "<strong>". $key . ":</strong> ". $data ."<br />";
 		}
 		echo "<br />";
-	}
-	
+	}	
 	public function redirect($url, $headers = 0, $long = 0) { # Redirects user, default headers
 		if(!$headers) {
 			header("Location: ". $url);	# Redirect with headers
 		} else {
 			echo '<meta http-equiv="REFRESH" content="'.$long.';url='.$url.'">'; # HTML Headers
-		}
+		}
 	}
 		/**	 *  Shows error default, sets error if $error set	 */
 	public function errors($error = 0) {		
@@ -40,8 +39,7 @@ if(THT != 1){die();}class main {
 			}
 		} else {
 			$_SESSION['errors'] = $error;
-		}			}
-	
+		}			}	
 	public function table($header, $content = 0, $width = 0, $height = 0) { # Returns the HTML for a THT table
 		global $style;
 		if($width) {
@@ -73,15 +71,13 @@ if(THT != 1){die();}class main {
 		}
 		return $tbl;
 	}
-	
 	public function evalreturn($code) { # Evals code and then returns it without showing
 		ob_start();
 		eval("?> " . $code . "<?php ");
 		$data = ob_get_contents();
 		ob_clean();
 		return $data;
-	}
-	
+	}	
 	public function done() { # Redirects the user to the right part
 		global $main;
 		foreach($main->getvar as $key => $value) {
@@ -94,13 +90,11 @@ if(THT != 1){die();}class main {
 				$url .= $i . $key . "=" . $value;
 			}		}
 		$main->redirect($url);
-	}
-	
+	}	
 	public function check_email($email) {
 		if($this->validEmail($email)) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
@@ -120,7 +114,7 @@ if(THT != 1){die();}class main {
 			$html .= '</select>';
 		}
 		return $html;
-	}		/**	 * New simpler version of the dropDown function	 * @param 	string	name of the select tag	 * @param	array	values with this structure array(1=>'Item 1', 2=>'Item 2')	 * @param 	array	extra information to add in the select i.e onclick, onBlur, etc	 * @param	bool	show or not a blank item	 * @return	html	returns the select html  	 */	public function createSelect($name, $values, $default = 0, $parameter_list = array(), $show_blank_item = true) {				$extra = '';		foreach($parameter_list as $key=>$parameter) {			$extra .= $key.'="'.$parameter.'"';		}		$html .= '<select name="'.$name.'" id="'.$name.'" '.$extra.'>';			if ($show_blank_item) {			$html .= '<option value="">-- Select --</option>';		}		if($values) {			foreach($values as $key => $value) {				$html .= '<option value="'.$key.'"';				if($default == $key) {					$html .= 'selected="selected"';				}				$html .= '>'.$value.'</option>';			}		}				$html .= '</select>';				return $html;	}			
+	}		/**	 * New simpler version of the dropDown function	 * @param 	string	name of the select tag	 * @param	array	values with this structure array(1=>'Item 1', 2=>'Item 2')	 * @param 	array	extra information to add in the select i.e onclick, onBlur, etc	 * @param	bool	show or not a blank item	 * @return	html	returns the select html  	 */	public function createSelect($name, $values, $default = 0, $parameter_list = array(), $show_blank_item = true) {				$extra = '';		foreach($parameter_list as $key=>$parameter) {			$extra .= $key.'="'.$parameter.'"';		}		$html .= '<select name="'.$name.'" id="'.$name.'" '.$extra.'>';			if ($show_blank_item) {			$html .= '<option value="">-- Select --</option>';		}		if($values) {			foreach($values as $key => $value) {				$html .= '<option value="'.$key.'"';				if($default == $key) {					$html .= 'selected="selected"';				}				$html .= '>'.$value.'</option>';			}		}				$html .= '</select>';				return $html;	}		
 	public function folderFiles($link) { # Returns the filenames of a content in a folder
 		$folder = $link;
 		if ($handle = opendir($folder)) { # Open the folder
@@ -132,7 +126,7 @@ if(THT != 1){die();}class main {
 		}
 		closedir($handle); #Close the folder
 		return $values;
-	}
+	}
 	public function checkIP($ip) { # Returns boolean for ip. Checks if exists
 		global $db;
 		global $main;
@@ -166,8 +160,8 @@ if(THT != 1){die();}class main {
 			}		}
 	}		/**	 * Checks the credentails of the client and logs in, returns true or false	 * 	 */
 	public function clientLogin($username, $pass) {
-		global $db, $main, $user;		$ip   = $_SERVER['REMOTE_ADDR'];			
-		if(isset($user) && isset($pass)) {			$user_info	= $user->getUserByUserName($username);						if (is_array($user_info) && !empty($user_info)) {				if ($user_info['status'] == USER_STATUS_ACTIVE) {										if(md5(md5($pass).md5($user_info['salt'])) == $user_info['password']) {												$_SESSION['clogged'] 	= 1;											$data['password'] 		= null;						$data['salt'] 			= null;						//Save all user in this session						$_SESSION['cuser'] 		= $user_info;						$this->addLog("Login successful $ip - $username");																							return true;					} else { 						$main->errors('Incorrect password!');					}				} else {					$main->errors('Your account is not active');				}			} else {				$main->errors('User does not exist');			}		}				$this->addLog("Login failed ($ip) - $username");			return false;	}
+		global $db, $main, $user;		$ip   = $_SERVER['REMOTE_ADDR'];			
+		if(isset($user) && isset($pass)) {			$user_info	= $user->getUserByUserName($username);						if (is_array($user_info) && !empty($user_info)) {				if ($user_info['status'] == USER_STATUS_ACTIVE) {										if(md5(md5($pass).md5($user_info['salt'])) == $user_info['password']) {												$_SESSION['clogged'] 	= 1;											$data['password'] 		= null;						$data['salt'] 			= null;						//Save all user in this session						$_SESSION['cuser'] 		= $user_info;						$this->addLog("Login successful $ip - $username");																							return true;					} else { 						$main->errors('Incorrect password!');					}				} else {					$main->errors('Your account is not active');				}			} else {				$main->errors('User does not exist');			}		}				$this->addLog("Login failed ($ip) - $username");			return false;	}
 	public function staffLogin($user, $pass) { # Checks the credentials of a staff member and returns true or false
 		global $db, $main;		$ip = $_SERVER['REMOTE_ADDR'];		$date = time();		
 		if($user && $pass) {
@@ -177,9 +171,9 @@ if(THT != 1){die();}class main {
 			} else {
 				$data = $db->fetch_array($query);
 				if(md5(md5($main->postvar['pass']) . md5($data['salt'])) == $data['password']) {
-					$_SESSION['logged'] = 1;					$data['password'] 	= null;					$data['salt'] 		= null;					$_SESSION['user'] 	= $data;										$main->addLog("STAFF LOGIN SUCCESSFUL ($ip)");					
+					$_SESSION['logged'] = 1;					$data['password'] 	= null;					$data['salt'] 		= null;					$_SESSION['user'] 	= $data;										$main->addLog("STAFF LOGIN SUCCESSFUL ($user, $ip)");					
 					return true;
-				} else {										$main->addLog("STAFF LOGIN FAILED ($ip)");
+				} else {										$main->addLog("STAFF LOGIN FAILED ($user, $ip)");
 					return false;
 				}
 			}
@@ -188,13 +182,11 @@ if(THT != 1){die();}class main {
 		}
 	}
 	
-	public function laterMonth($num) { # Makes the date with num of months after current
-		$day = date('d');
-		$month = date('m');
-		$year = date('Y');
-		
-		$endMonth = $month + $num;
-		
+	public function laterMonth($num) { # Makes the date with num of months after current
+		$day = date('d');
+		$month = date('m');
+		$year = date('Y');
+		$endMonth = $month + $num;
 		switch($endMonth) {
 		case 1:
 		$year++;
@@ -205,16 +197,15 @@ if(THT != 1){die();}class main {
 		{
 		// check if the year is leap
 		$day = 28; // or you can keep the day and increase the month
+		}
 		}
-		}
-		break;
+		break;
 		default:
 		// nothing to do 
-		break;
-		}
-		
-		return mktime(0,0,0,$endMonth,$day,$year);
-	}
+		break;
+		}
+		return mktime(0,0,0,$endMonth,$day,$year);
+	}
 	
 	/**
 	* Validate an email address.
@@ -224,14 +215,12 @@ if(THT != 1){die();}class main {
 	* Thank you, Linux Journal!
 	* http://www.linuxjournal.com/article/9585
 	*/
-	public function validEmail($email) {
+	public function validEmail($email) {
 	   $isValid = true;
 	   $atIndex = strrpos($email, "@");
 	   if (is_bool($atIndex) && !$atIndex)   {
 		  $isValid = false;
-	   }
-	   else
-	   {
+	   }   else   {
 		  $domain = substr($email, $atIndex+1);
 		  $local = substr($email, 0, $atIndex);
 		  $localLen = strlen($local);
@@ -311,4 +300,4 @@ if(THT != 1){die();}class main {
 		
 		//Let's wrap it all up.
 		return true;
-	}		public function	countrySelect($selected_item = '') {		//@todo this will be move into other place something like includes/library/text.lib.php		require_once 'text.php';		$selected_item = strtoupper($selected_item);		return $this->createSelect('country', $country, $selected_item);	}		// Order status	public function getOrderStatusList() {		return array(			ORDER_STATUS_ACTIVE						=> 'Active', 			ORDER_STATUS_WAITING_USER_VALIDATION 	=> 'Waiting user validation',						ORDER_STATUS_WAITING_ADMIN_VALIDATION	=> 'Waiting admin validation',			ORDER_STATUS_CANCELLED 					=> 'Cancelled',  			//ORDER_STATUS_WAITING_PAYMENT			=> 'Waiting payment', 			ORDER_STATUS_DELETED					=> 'Deleted', 			);	}		public function getInvoiceStatusList() {		return array(			INVOICE_STATUS_PAID				=> 'Paid', 			INVOICE_STATUS_CANCELLED		=> 'Cancelled',						INVOICE_STATUS_WAITING_PAYMENT	=> 'Pending', 			INVOICE_STATUS_DELETED			=> 'Deleted'			);	}		public function getUserStatusList() {		return array(			USER_STATUS_ACTIVE						=> 'Active', 			USER_STATUS_SUSPENDED 					=> 'Suspend', 			USER_STATUS_WAITING_ADMIN_VALIDATION	=> 'Waiting admin validation',  			//USER_STATUS_WAITING_PAYMENT				=> 'Waiting payment',  //should be remove only added for backward comptability			USER_STATUS_DELETED						=> 'Deleted', 			);	}		/**	 * Gets current user info 	 */	public function getCurrentUserInfo() {		if (isset($_SESSION['cuser']) && is_array($_SESSION['cuser'])) {			return $_SESSION['cuser'];		} else {			return false;		}	}		/**	 * Gets the curren user id 	 */	public function getCurrentUserId() {		if (isset($_SESSION['cuser']) && is_array($_SESSION['cuser'])) {			return intval($_SESSION['cuser']['id']);		} else {			return false;		}	}			/**	 * Gets current staff info 	 */	public function getCurrentStaffInfo() {		if (isset($_SESSION['user']) && is_array($_SESSION['user'])) {			return $_SESSION['user'];		} else {			return false;		}	}		/**	 * Gets the curren staff id 	 */	public function getCurrentStaffId() {		if (isset($_SESSION['user']) && is_array($_SESSION['user'])) {			return intval($_SESSION['user']['id']);		} else {			return false;		}	}		/**	 * Gets the admin menu 	 */	public function getAdminMenu() {				if (isset($_SESSION['admin_menu']) && count($_SESSION['admin_menu']) > 0 ) {			return $_SESSION['admin_menu'];					} else {			global $db;						$result = $db->query("SELECT * FROM `<PRE>acpnav`");			$menu_list = $db->store_result($result);			$new_menu_list = array();			foreach($menu_list as $menu) {				$new_menu_list[$menu['link']] = $menu;			}						$_SESSION['admin_menu'] = $new_menu_list;			return $menu_list;		}	}		/**	 * Check if the link exist in the admin menu	 * @param	string	link 	 * @return  bool	true if sucess		 */	public function linkAdminMenuExists($link) {		$list = $this->getAdminMenu();		if (isset($list[$link]) && $list[$link]['link'] == $link) {			return true;		} else {			//somebody is trying to hack			return false;		}	}		/**	 * Gets the list of subdomains by server id	 * @param	int		server id		 * @return	array	list of subdomains	 */	public function getSubDomainByServer($server_id) {		global $db;		$result = $db->query("SELECT id, subdomain FROM `<PRE>subdomains` WHERE `server` = '{$server_id}' ");		$array = array();		while($data = $db->fetch_array($result, 'ASSOC')) {			$array[$data['id']] = $data['subdomain'];		}			return $array;			}			function toDate($d, $format= 'Y-m-d') {				if (strtotime($d) === false || strtotime($d) === -1)			return $d;		return date($format, strtotime($d));	}		function getDateArray($date) {		return getdate(strtotime($date));	}		public function generatePassword() {		for ($digit = 0; $digit < 5; $digit++) {			$r = rand(0,1);			$c = ($r==0)? rand(65,90) : rand(97,122);			$passwd .= chr($c);		}		return $passwd;	}		public function generateUsername() {		$t = rand(5,8);		for ($digit = 0; $digit < $t; $digit++) {			$r = rand(0,1);			$c = ($r==0)? rand(65,90) : rand(97,122);			$user .= chr($c);		}		return $user;	}				public function insertLog($user_id, $user_name, $message) {		global $db;		if (empty($user_id)) {			$user_id = '0';		}		if (empty($user_name)) {			$user_name = 'Anonymous';		}		$date = time();				$db->query("INSERT INTO `<PRE>logs` (uid, loguser, logtime, message) VALUES(														'{$user_id}',														'{$user_name}',														'{$date}',														'{$message}')");			}				public function addLog($message) {		global $db;				$date = time();				if($this->getCurrentStaffId() != false) {			$user_id   	= $this->getCurrentStaffId();			$user_name 	= $this->getCurrentStaffInfo();			$user_name 	= $user_name['user'];		} elseif ($this->getCurrentUserId() != false) {			$user_id	= $this->getCurrentUserId();			$user_name 	= $this->getCurrentUserInfo();			$user_name 	= $user_name['user'];		} elseif (CRON == 1) {			$user_id = '0';			$user_name = 'Cron';		} else {			$user_id = '0';			$user_name = 'Anonymous';		}						$db->query("INSERT INTO `<PRE>logs` (uid, loguser, logtime, message) VALUES(			'{$user_id}',			'{$user_name}',			'{$date}',			'{$message}')");								}	}
+	}		/**	 * Generates a contry select	 * @param	string	code of the country	 * @return	string	html content	 */	public function	countrySelect($selected_item = '') {		//@todo this will be move into other place something like includes/library/text.lib.php		require_once 'text.php';		$selected_item = strtoupper($selected_item);		return $this->createSelect('country', $country, $selected_item);	}		// Order status	public function getOrderStatusList() {		return array(			ORDER_STATUS_ACTIVE						=> 'Active', 			ORDER_STATUS_WAITING_USER_VALIDATION 	=> 'Waiting user validation',						ORDER_STATUS_WAITING_ADMIN_VALIDATION	=> 'Waiting admin validation',			ORDER_STATUS_CANCELLED 					=> 'Cancelled',  			//ORDER_STATUS_WAITING_PAYMENT			=> 'Waiting payment', 			ORDER_STATUS_DELETED					=> 'Deleted', 			);	}		public function getInvoiceStatusList() {		return array(			INVOICE_STATUS_PAID				=> 'Paid', 			INVOICE_STATUS_CANCELLED		=> 'Cancelled',						INVOICE_STATUS_WAITING_PAYMENT	=> 'Pending', 			INVOICE_STATUS_DELETED			=> 'Deleted'			);	}		public function getUserStatusList() {		return array(			USER_STATUS_ACTIVE						=> 'Active', 			USER_STATUS_SUSPENDED 					=> 'Suspend', 			USER_STATUS_WAITING_ADMIN_VALIDATION	=> 'Waiting admin validation',  			//USER_STATUS_WAITING_PAYMENT				=> 'Waiting payment',  //should be remove only added for backward comptability			USER_STATUS_DELETED						=> 'Deleted', 			);	}		/**	 * Gets current user info 	 */	public function getCurrentUserInfo() {		if (isset($_SESSION['cuser']) && is_array($_SESSION['cuser'])) {			return $_SESSION['cuser'];		} else {			return false;		}	}		/**	 * Gets the curren user id 	 */	public function getCurrentUserId() {		if (isset($_SESSION['cuser']) && is_array($_SESSION['cuser'])) {			return intval($_SESSION['cuser']['id']);		} else {			return false;		}	}			/**	 * Gets current staff info 	 */	public function getCurrentStaffInfo() {		if (isset($_SESSION['user']) && is_array($_SESSION['user'])) {			return $_SESSION['user'];		} else {			return false;		}	}		/**	 * Gets the curren staff id 	 */	public function getCurrentStaffId() {		if (isset($_SESSION['user']) && is_array($_SESSION['user'])) {			return intval($_SESSION['user']['id']);		} else {			return false;		}	}		/**	 * Gets the admin menu 	 */	public function getAdminMenu() {				if (isset($_SESSION['admin_menu']) && count($_SESSION['admin_menu']) > 0 ) {			return $_SESSION['admin_menu'];					} else {			global $db;						$result = $db->query("SELECT * FROM `<PRE>acpnav`");			$menu_list = $db->store_result($result);			$new_menu_list = array();			foreach($menu_list as $menu) {				$new_menu_list[$menu['link']] = $menu;			}						$_SESSION['admin_menu'] = $new_menu_list;			return $menu_list;		}	}		/**	 * Check if the link exist in the admin menu	 * @param	string	link 	 * @return  bool	true if sucess		 */	public function linkAdminMenuExists($link) {		$list = $this->getAdminMenu();		if (isset($list[$link]) && $list[$link]['link'] == $link) {			return true;		} else {			//somebody is trying to hack			return false;		}	}		/**	 * Gets the list of subdomains by server id	 * @param	int		server id		 * @return	array	list of subdomains	 */	public function getSubDomainByServer($server_id) {		global $db;		$result = $db->query("SELECT id, subdomain FROM `<PRE>subdomains` WHERE `server` = '{$server_id}' ");		$array = array();		while($data = $db->fetch_array($result, 'ASSOC')) {			$array[$data['id']] = $data['subdomain'];		}			return $array;			}			function toDate($d, $format= 'Y-m-d') {				if (strtotime($d) === false || strtotime($d) === -1)			return $d;		return date($format, strtotime($d));	}		function getDateArray($date) {		return getdate(strtotime($date));	}		/**	 * Generates a random password	 */	public function generatePassword() {		for ($digit = 0; $digit < 5; $digit++) {			$r = rand(0,1);			$c = ($r==0)? rand(65,90) : rand(97,122);			$passwd .= chr($c);		}		return $passwd;	}		/**	 * Generates a random username	 */	public function generateUsername() {		$t = rand(5,8);		for ($digit = 0; $digit < $t; $digit++) {			$r = rand(0,1);			$c = ($r==0)? rand(65,90) : rand(97,122);			$user .= chr($c);		}		return $user;	}		/**	 * Adds a log	 * @param	string	message to save in the log	 */			public function addLog($message) {		global $db;				$date = time();		//Tries to save the log as a staff user				if($this->getCurrentStaffId() != false) {			$user_id   	= $this->getCurrentStaffId();			$user_name 	= $this->getCurrentStaffInfo();			$user_name 	= $user_name['user'];		} elseif ($this->getCurrentUserId() != false) {			$user_id	= $this->getCurrentUserId();			$user_name 	= $this->getCurrentUserInfo();			$user_name 	= $user_name['user'];		} elseif (CRON == 1) {			$user_id = '0';			$user_name = 'Cron';		} else {			$user_id = '0';			$user_name = 'Anonymous';		}				$db->query("INSERT INTO `<PRE>logs` (uid, loguser, logtime, message) VALUES(			'{$user_id}',			'{$user_name}',			'{$date}',			'{$message}')");								}	}
